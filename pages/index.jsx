c435b462771858bb7da72b7e68aafeba0366d3e6
Gustavo Faria <gustavo.fariams@outlook.com>
2023-05-05 11:17:07 -0300

    start

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Page from "../src/components/page/Page"
+
+
+function HomePage () {
+  
+  return (
+    <div>
+      <Page />
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/page/Page.jsx
@@ -0,0 +1,14 @@
+import styles from './Page.module.css'
+
+function Page() {
+    return(
+        <div className={styles.header}>
+            dsaiodhjaiodnjaisdnainds
+        </div>
+
+    )
+  
+}
+
+
+export default Page
