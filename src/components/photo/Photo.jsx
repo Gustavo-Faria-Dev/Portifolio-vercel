23f458337a068d076b921e0483ac75474787431f
Gustavo Faria <gustavo.fariams@outlook.com>
2023-05-05 17:06:30 -0300

    media photo

--- src/components/photo/Photo.jsx
+++ src/components/photo/Photo.jsx
@@ -5,9 +5,23 @@ function Photo(){
     return(
         <div className = {styles.photo}>
             <img  style={{
-                width: 300
+                width: 300,
+                borderRadius: 500
 
             }} src = '/images/eu.jpeg' />
+
+            <div className={styles.sobre}>
+            <h1>
+                Sobre
+            </h1>
+            
+            <p>
+
+            Me formei em 2021 no curso de engenharia de materiais e atuei no setor de compras de uma multinacional, líder em seu setor. 
+            Em 2022 decidi seguir minha paixão e aprofundar meus conhecimentos na área de programação. Atualmente desenvolvo aplicações Web utilizando tecnologias como React, JavaScript, HTML e CSS.
+            </p>
+            </div>
+
         </div>
     )
 }
