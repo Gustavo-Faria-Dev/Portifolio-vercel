471d08d5f4a6ca0b2138f965464f8c45e38a2106
Gustavo Faria <gustavo.fariams@outlook.com>
2023-05-06 09:38:14 -0300

    link github

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -3,15 +3,26 @@ import styles from './Contacts.module.css'
 
 function Contacts(){
     return(
-
+        
+      
         <div id= "contacts" className={styles.contacts}>
-            <h1>Contacts</h1>
+        <div>
+        <h1>
+            Contacts
+        </h1>
             
-            <p>
-                dsoamdoiamsdimamdmamdaimsdmaidimaisdias
-
-            </p>
+        
+        <a href = "https://github.com/Gustavo-Faria-Dev" target="_blank"> 
+            <img src = {`/images/github.svg`}/> 
+            
+        </a>
+        
+    
         </div>
+       
+        </div>    
+    
+    
     )
 
 
